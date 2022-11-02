14cf472f70438f45559134834c68156add80bcf1
Leonardo Tricotti <tricottileo@gmail.com>
2022-11-02 20:23:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { NavBar } from "./components/NavBar/NavBar";
 import { Card } from "./components/Items/ItemListContainer";
+import { Filter } from "./components/Filter/Filter";
 
 export const App = () => {
   return (
     <>
       <NavBar />
+      <Filter />
       <Card />
     </>
   );
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,21 @@
+import img from "../../assets/images/mujer/calzado/banner/0047b494219b8af236da089e2c86.jpg";
+import styles from "./filter.module.css";
+import { PlusSign } from "../Widgets/PlusSign";
+import { MinusSign } from "../Widgets/MinusSign";
+
+export const Filter = () => {
+  return (
+    <div className={styles.bannerImg}>
+      <img src={img} alt="calzado mujeres" />
+      <div className={styles.filterBtn}>
+        <span>Filtros</span>
+        <div className={styles.plusSign}>
+          <PlusSign />
+        </div>
+        <div className={styles.minusSign}>
+          <MinusSign />
+        </div>
+      </div>
+    </div>
+  );
+};
