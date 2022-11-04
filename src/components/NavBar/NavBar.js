ec9c97fe7b84e98b3e8004c2d91262ba2681d10a
Leonardo Tricotti <tricottileo@gmail.com>
2022-11-04 09:31:56 -0300

    first commit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,11 +6,12 @@ import { NavToggle } from "../Widgets/NavToggle";
 import { useState } from "react";
 
 export function NavBar() {
+  const [toggle, setToggle] = useState(false)
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.navToggle}>
-        <NavToggle />
+        <NavToggle action={() => setToggle(!toggle)} />
       </div>
       <div className={styles.navRight}>
         <div className={styles.logoContainer}>
@@ -18,19 +19,21 @@ export function NavBar() {
             <img src={logo} alt="Logo" className={styles.headerLogo} />
           </a>
         </div>
-        <nav className={styles.navContainer}>
-          <ul className={styles.navItem}>
-            {MenuItems.map((item) => {
-              return (
-                <li key={item.title} className={styles.navList}>
-                  <a href={item.url} className={styles.navLink}>
-                    {item.title}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+        {toggle &&
+          <nav className={styles.navContainer}>
+            <ul className={styles.navItem}>
+              {MenuItems.map((item) => {
+                return (
+                  <li key={item.title} className={styles.navList}>
+                    <a href={item.url} className={styles.navLink}>
+                      {item.title}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+      }
       </div>
       <div className={styles.cart}>
         <p>Cart</p>
--- src/components/Widgets/NavToggle.js
+++ src/components/Widgets/NavToggle.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-export function NavToggle() {
-  return <FontAwesomeIcon icon={faBars} />
+export function NavToggle({ action }) {
+  return <FontAwesomeIcon onClick={action} icon={faBars} />
 }
