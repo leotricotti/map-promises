f646cb0ba5781f23449daecf6c0e9908e73ab1a4
Leonardo Tricotti <tricottileo@gmail.com>
2022-11-04 09:58:53 -0300

    first commit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,10 +3,24 @@ import { MenuItems } from "./MenuItems";
 import styles from "./NavBar.module.css";
 import { CartWidget } from "../Widgets/CartWidget";
 import { NavToggle } from "../Widgets/NavToggle";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function NavBar() {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+
+  //Funcion que maneja el estado de acuerdo al tamaño de pantalla
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 992) {
+        setToggle(true);
+      } else {
+        setToggle(false);
+      }
+    }
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+  });
 
   return (
     <header className={styles.headerContainer}>
@@ -19,7 +33,7 @@ export function NavBar() {
             <img src={logo} alt="Logo" className={styles.headerLogo} />
           </a>
         </div>
-        {toggle &&
+        {toggle && (
           <nav className={styles.navContainer}>
             <ul className={styles.navItem}>
               {MenuItems.map((item) => {
@@ -33,7 +47,7 @@ export function NavBar() {
               })}
             </ul>
           </nav>
-      }
+        )}
       </div>
       <div className={styles.cart}>
         <p>Cart</p>
